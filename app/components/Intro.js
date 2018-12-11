c787a1b613473e59d992b14ccd83bca4b475b981
Vasilchikov Aleksey <alekseyvi1303@gmail.com>
2018-12-11 13:24:37 +0300

    fix: bug с редиректом

--- app/components/Intro.js
+++ app/components/Intro.js
@@ -113,8 +113,8 @@ class Intro extends Component {
 								<img src="/img/logo.png" />
 							</figure>
 							<ul>
-								<li><Link onClick={this.handleClick} name="mfo" className={url === "/mfo" ? "active" : ""} to="mfo">Микрозаймы</Link></li>
-								<li><Link onClick={this.handleClick} name="cards" className={url === "/cards" ? "active" : ""} to="cards">Кредитные карты</Link></li>
+								<li><Link onClick={this.handleClick} name="mfo" className={url === "/mfo" ? "active" : ""} to="/mfo">Микрозаймы</Link></li>
+								<li><Link onClick={this.handleClick} name="cards" className={url === "/cards" ? "active" : ""} to="/cards">Кредитные карты</Link></li>
 								{/*<li><Link className={url === "/credits" ? "active" : ""} to="credits">Кредиты</Link></li>*/}
 							</ul>
 						</header>
